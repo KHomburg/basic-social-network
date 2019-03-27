e2b1f887fd58312569f20bd5e57b5ee37dff7484
KHomburg <kevin.homburg@outlook.de>
2019-03-27 23:11:09 +0100

    created routes to check for Notification and query Notifications

--- routes/notification.js
+++ routes/notification.js
@@ -14,31 +14,70 @@ const ContentImage = require("../models/Contentimage");
 //get notifications
 router.get("/test", authenticate.checkLogIn, authenticate.reqSessionProfile, (req, res) => { 
     const currentUserProfile = req.currentUserProfile
-    const userID = currentUserProfile._id
-    console.log(userID)
-    //const addressees = Notification.addressee
-    //notification
-    //Notification.find({currentUserProfile: { $in: addressees._id}})
-    //    .populate(        
-    //        [
-    //            {
-    //                path: "profile",
-    //                model: "profile"
-    //            },
-    //            {
-    //                path: "group",
-    //                model: "group"
-    //            },
-    //        ]
-    //    )
-    //    .exec((err, notifications) => {
-    //        res.json(notifications);
-    //    })
-
     
     Notification.find({"addressee._id": currentUserProfile})
-        .populate('refContent')
+        .populate(
+            [
+                {
+                    path: 'refContent'
+                }
+            ]
+        )
         .exec((err, notifications) => {
+            res.json(notifications);
+        })
+
+});
+
+
+//check wether the User has new notifications
+//get /notification/check (private)
+router.get("/check", authenticate.checkLogIn, authenticate.reqSessionProfile, (req, res) => { 
+    const currentUserProfile = req.currentUserProfile
+    
+    Notification.findOne({"addressee._id": currentUserProfile})
+        .sort({lastUpdated: 'desc'})
+        .where("updatedBy").ne(currentUserProfile) //exclude notification that have been updated latest by currentUser
+        .exec((err, notification) => {
+            console.log(currentUserProfile.notificationCheck + " test")
+            console.log(notification.lastUpdated)
+            if(notification){
+                //check if latest notification updated later than profilenotificationCheck date
+                let alert
+                if(currentUserProfile.notificationCheck > notification.lastUpdated){
+                    alert = "false"
+                } else {
+                    alert = "true"
+                }
+                
+                res.send(notification + " ALERT: " + alert);
+            }else{
+                res.send("no Notifications");
+            }
+        })
+
+});
+
+//query the 5 most recent updated notification for currentUserProfile as addressee
+//get /notification/check (private)
+router.get("/notifications", authenticate.checkLogIn, authenticate.reqSessionProfile, (req, res) => { 
+    const currentUserProfile = req.currentUserProfile
+    
+    Notification.find({"addressee._id": currentUserProfile})
+        .sort('-date')
+        //.where("updatedBy").ne(currentUserProfile) //exclude notification that have been updated latest by currentUser
+        .limit(5)
+        //.populate(
+        //    [
+        //        {
+        //            path: 'refContent'
+        //        }
+        //    ]
+        //)
+        .exec((err, notifications) => {
+            //update notificationCheck date
+            currentUserProfile.notificationCheck = new Date()
+            currentUserProfile.save()
             res.json(notifications);
         })
 
